2e15599a5fbff09507520ecef44663351d52402e
Anton <antklim89@mail.ru>
2023-07-19 15:55:56 +0300

    feat: add container

--- features/ControlPanel/ControlPanel.tsx
+++ features/ControlPanel/ControlPanel.tsx
@@ -3,8 +3,10 @@ import { ReactNode } from 'react';
 
 const ControlPanel = ({ children }: {children: ReactNode}) => {
     return (
-        <div className="flex bg-secondary py-4 px-2">
-            {children}
+        <div className="bg-secondary">
+            <div className="container m-auto flex py-4 bpx-2">
+                {children}
+            </div>
         </div>
     );
 };
