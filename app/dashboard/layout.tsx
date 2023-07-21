9b334e727726d7ecb06d7f3f35cd275be3f8955e
Anton <antklim89@mail.ru>
2023-07-21 20:57:18 +0300

    feat: install supabase

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 
 const DashboardLayout = ({ login, children }: { login: ReactNode, children: ReactNode }) => {
-    const isAuth = true;
+    const isAuth = false;
 
     if (isAuth) return children;
     return login;
--- constants.ts
+++ constants.ts
@@ -1,1 +1,8 @@
+import { z } from 'zod';
+
+
 export const DEFAULT_TITLE = process.env.DEFAULT_TITLE || 'Tasks manager';
+
+
+export const SUPABASE_ANON_KEY = z.string().parse(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+export const SUPABASE_URL = z.string().parse(process.env.NEXT_PUBLIC_SUPABASE_URL);
