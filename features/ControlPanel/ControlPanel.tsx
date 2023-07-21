cc9ab5999f23bc980dbbf7c1d0e4a7c6038739ee
Anton <antklim89@mail.ru>
2023-07-21 19:48:55 +0300

    style: remove bg

--- features/ControlPanel/ControlPanel.tsx
+++ features/ControlPanel/ControlPanel.tsx
@@ -3,10 +3,8 @@ import { ReactNode } from 'react';
 
 const ControlPanel = ({ children }: {children: ReactNode}) => {
     return (
-        <div className="bg-secondary">
-            <div className="container m-auto flex py-4 bpx-2">
-                {children}
-            </div>
+        <div className="container m-auto flex py-4 bpx-2">
+            {children}
         </div>
     );
 };
