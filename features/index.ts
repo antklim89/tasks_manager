62f4358f961be1ea9787db5f6363adcb83b6768f
Anton <antklim89@mail.ru>
2023-07-30 18:26:03 +0300

    feat: remove ControlPanel

--- features/index.ts
+++ features/index.ts
@@ -5,5 +5,4 @@ export { default as NewProject } from './NewProject';
 export { default as Hero } from './Hero';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
-export { default as ControlPanel } from './ControlPanel';
 export { default as Auth } from './Auth';
