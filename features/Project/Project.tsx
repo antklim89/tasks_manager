7b3e99daee778fb1b1bf9b3c5af88d0b1ed099f0
Anton <antklim89@mail.ru>
2023-08-05 10:36:41 +0300

    feat: add loading state

--- features/Project/Project.tsx
+++ features/Project/Project.tsx
@@ -5,12 +5,13 @@ import { useCreateColumn, useFetchColumns } from '@/requests';
 
 const Project = ({ projectId }: {projectId: number}) => {
     const { trigger: createColumn, isMutating } = useCreateColumn();
-    const { data: columns = [] } = useFetchColumns({ projectId });
+    const { data: columns = [], isLoading } = useFetchColumns({ projectId });
 
     const handleCreateColumn = () => {
         createColumn({ projectId });
     };
 
+    if (isLoading) return <span className="loading loading-bars loading-lg" />;
     return (
         <div className="flex gap-2">
             {columns.map((column) => (
