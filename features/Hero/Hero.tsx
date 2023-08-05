8c3f167bc04d9ac345f3b47f725f3b15a2e5296e
Anton <antklim89@mail.ru>
2023-08-05 10:29:20 +0300

    style: remove bg

--- features/Hero/Hero.tsx
+++ features/Hero/Hero.tsx
@@ -4,16 +4,16 @@ import Link from 'next/link';
 
 const Hero = () => {
     return (
-        <div className="hero h-full bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
-                <div>
+        <div className="hero h-full">
+            <div className="hero-content max-w-full flex-col lg:flex-row">
+                <div className="flex-1">
                     <h1 className="text-5xl font-bold">Manage your tasks!</h1>
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <Link className="btn btn-primary" href="/dashboard">Get Started</Link>
                 </div>
                 <Image
                     alt="hero"
-                    className="w-full rounded-lg shadow-2xl"
+                    className="flex-1 rounded-lg shadow-2xl"
                     height={480}
                     src="/hero.jpg"
                     width={720}
