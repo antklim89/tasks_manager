a0b4f9c65536a817e4af23bf620461f923be67c2
Anton <antklim89@mail.ru>
2023-08-07 14:05:25 +0300

    feat: add global swr config

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+import { SWRConfig } from 'swr';
+
 import { Hero } from '@/features';
 
 
 const HomePage = async () => {
+    const swrConfig: Parameters<typeof SWRConfig>[0]['value'] = {
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnMount: true,
+        revalidateOnReconnect: false,
+    } as const;
 
     return (
-        <>
+        <SWRConfig value={swrConfig}>
             <Hero />
-        </>
+        </SWRConfig>
     );
 };
 
