67242dccdd699e37a526e66a82b6365dfda4a0c4
Anton <antklim89@mail.ru>
2023-08-08 15:46:31 +0300

    feat: add populateCache

--- requests/useProjectCreate.ts
+++ requests/useProjectCreate.ts
@@ -1,23 +1,36 @@
 import useSWRMutation from 'swr/mutation';
 
 import { NewProjectType } from '@/features/NewProject/NewProject.schema';
+import { ProjectType, projectSchema } from '@/schemas';
 import { clientComponentClient, getClientComponentUser } from '@/utils';
 
-import { CreateProjectKey } from './keys';
+import { FetchProjectsKey } from './keys';
 
 
 export function useProjectCreate() {
-    return useSWRMutation<void, Error, CreateProjectKey, NewProjectType>(
-        ['CREATE_NEW_PROJECT'],
+    return useSWRMutation<ProjectType, Error, FetchProjectsKey, NewProjectType>(
+        ['PROJECTS'],
 
         async (key, { arg: { name } }) => {
             const supabase = clientComponentClient();
 
             const user = await getClientComponentUser();
 
-            const { error } = await supabase.from('projects').insert({ name, owner: user.id });
+            const { error, data } = await supabase.from('projects')
+                .insert({ name, owner: user.id })
+                .select('*')
+                .single();
+
             if (error?.code === '23505') throw new Error(`Project with name "${name}" alerady exists.`);
             if (error) throw new Error('Failed to add new project. Try again later.');
+
+            return projectSchema.parseAsync(data);
+        },
+        {
+            revalidate: false,
+            populateCache(newProject, currentProjects: ProjectType[]) {
+                return [...currentProjects, newProject];
+            },
         },
     );
 }
