8f2fef45c9b5c70fa1b39868dce73bb0f735cae3
Anton <antklim89@mail.ru>
2023-08-08 15:39:29 +0300

    feat: make client component

--- features/DashboardHome/DashboardHome.tsx
+++ features/DashboardHome/DashboardHome.tsx
@@ -1,19 +1,17 @@
+'use client';
 import Link from 'next/link';
 
-import { getServerComponentUser, serverComponentClient } from '@/utils';
+import { useProjectsFetch } from '@/requests';
 
 
-const DashboardHome = async () => {
-    const user = await getServerComponentUser();
-    const { data: projects } = await serverComponentClient()
-        .from('projects')
-        .select('id, name')
-        .eq('owner', user.id);
+const DashboardHome = () => {
+    const { data: projects, isLoading } = useProjectsFetch();
 
     return (
         <div>
-            <h2 className="text-4xl text-center">{user.email?.split('@')[0]}`s dashboard.</h2>
+            <h2 className="text-4xl text-center">Dashboard</h2>
             <h3>Select your project</h3>
+            {isLoading ? <span className="loading loading-bars loading-lg" /> : null}
             <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
                 {projects?.map((project) => (
                     <Link className="card bg-base-100 shadow-xl hover:bg-base-200 transition" href={`/dashboard/${project.id}`} key={project.id}>
--- features/SelectProject/SelectProject.tsx
+++ features/SelectProject/SelectProject.tsx
@@ -1,21 +1,17 @@
+'use client';
 import Link from 'next/link';
 
 import { Button } from '@/components';
-import { getServerComponentUser, serverComponentClient } from '@/utils';
+import { useProjectsFetch } from '@/requests';
 
 
-const SelectProject = async ({ projectId }: {projectId?: number}) => {
-    const user = await getServerComponentUser();
-
-    const { data: projects } = await serverComponentClient()
-        .from('projects')
-        .select('id, name')
-        .eq('owner', user.id);
+const SelectProject = ({ projectId }: {projectId?: number}) => {
+    const { data: projects, isLoading } = useProjectsFetch();
     const selectedProject = projects?.find((project) => project.id === projectId);
 
     return (
         <div className="dropdown">
-            <Button className="btn-outline" tabIndex={0}>{selectedProject?.name || 'Select Project'}</Button>
+            <Button className="btn-outline" isLoading={isLoading} tabIndex={0}>{selectedProject?.name || 'Select Project'}</Button>
             <ul className="dropdown-content z-[1] menu p-2 shadow bg-base-200 rounded-box w-52">
                 {projects?.map((project) => (
                     <li key={project.id}><Link href={`/dashboard/${project.id}`}>{project.name}</Link></li>
