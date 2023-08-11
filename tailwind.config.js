b8fa0d3221fd05bf6ce089b7d793fd90e927c15c
Anton <antklim89@mail.ru>
2023-08-11 20:34:07 +0300

    style: update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
@@ -10,7 +11,28 @@ module.exports = {
         extend: {},
     },
     daisyui: {
-        themes: ['business'],
+        themes: [
+            {
+                dark: {
+                    'base-100': '#111827',
+                    'base-200': '#1f2937',
+                    'base-300': '#332334',
+                    'color-scheme': 'dark',
+                    'primary': '#1C4E80',
+                    'secondary': '#7C909A',
+                    'accent': '#EA6947',
+                    'neutral': '#23282E',
+                    'info': '#0091D5',
+                    'success': '#6BB187',
+                    'warning': '#DBAE59',
+                    'error': '#AC3E31',
+
+                    '--rounded-box': '0.25rem',
+                    '--rounded-btn': '.125rem',
+                    '--rounded-badge': '.125rem',
+                },
+            },
+        ],
     },
     plugins: [require('daisyui')],
 };
