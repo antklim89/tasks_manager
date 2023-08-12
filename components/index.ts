e786a6f0c20497f10baaf1d097f305440bf4c49c
Anton <antklim89@mail.ru>
2023-08-12 21:05:27 +0300

    feat: remove Dialog

--- components/index.ts
+++ components/index.ts
@@ -2,7 +2,6 @@
 
 export { default as Alert } from './Alert';
 export { default as Button } from './Button';
-export { default as Dialog } from './Dialog';
 export { default as Input } from './Input';
 export { default as Modal } from './Modal';
 export { default as SwrProvider } from './SwrProvider';
