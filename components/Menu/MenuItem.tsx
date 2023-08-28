b331ae59ee2ff4f28da0bc0da0260450fe6a15bc
Anton <antklim89@mail.ru>
2023-08-28 15:55:17 +0300

    style: change bg color

--- components/Menu/MenuItem.tsx
+++ components/Menu/MenuItem.tsx
@@ -3,7 +3,7 @@ import { MenuItemProps } from './Menu.types';
 
 const MenuItem = ({ children }: MenuItemProps) => {
     return (
-        <div className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+        <div className="dropdown-content z-[1] menu p-2 shadow bg-base-200 rounded-box w-52">
             {children}
         </div>
     );
