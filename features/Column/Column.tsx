0bd6c02b23485656ef1b4d3b4893b9299f251295
Anton <antklim89@mail.ru>
2023-08-31 21:38:32 +0300

    feat: update drag and drop

--- features/Column/Column.tsx
+++ features/Column/Column.tsx
@@ -1,16 +1,13 @@
 'use client';
-import { useDrop } from 'react-dnd';
 import { FaEllipsisVertical } from 'react-icons/fa6';
-import { useSWRConfig } from 'swr';
-import { twMerge } from 'tailwind-merge';
 
 import { Menu } from '@/components';
 import Task from '@/features/Task';
+import { useTaskDrop } from '@/hooks';
 import { useTasksFetch } from '@/requests';
-import { FetchTasksKey } from '@/requests/keys';
-import { ColumnType, TaskType } from '@/schemas';
+import { ColumnType } from '@/schemas';
+import { cn } from '@/utils';
 
-import { TaskDragItem } from '../Task/Task.types';
 
 import ColumnDelete from './ColumnDelete';
 import ColumnName from './ColumnName';
@@ -20,45 +17,29 @@ import ColumnTaskCreate from './ColumnTaskCreate';
 const Column = ({ id, name, project }: ColumnType) => {
     const { data: tasks = [], isLoading } = useTasksFetch({ columnId: id });
 
-    const { mutate } = useSWRConfig();
-    const [{ isOver }, ref] = useDrop({
-        accept: 'TASK',
-        collect: (monitor) => ({
-            isOver: monitor.isOver(),
-        }),
-        async drop(item) {
-            const dropedTask = item as TaskDragItem;
-            if (dropedTask.task.columnId === id) return;
-            await dropedTask.updateTask({ columnId: id });
-            mutate(
-                ['TASKS', { columnId: id }] satisfies FetchTasksKey,
-                (currentData?: TaskType[]) => [...(currentData || []), { ...dropedTask.task, columnId: id }],
-                { revalidate: false },
-            );
-        },
-    });
+    const { isOver, taskDropRef } = useTaskDrop({ columnId: id });
 
     return (
         <div className="card w-96 bg-base-200 shadow-xl">
-            <div className="card-title flex justify-between">
-                <ColumnName id={id} name={name} projectId={project} />
-                <Menu button={<button className="btn m-1" type="button"><FaEllipsisVertical /></button>}>
-                    <Menu.Item>
-                        <ColumnDelete id={id} projectId={project} />
-                    </Menu.Item>
-                </Menu>
-            </div>
-            <div className="card-body p-1">
-
-                <div className={twMerge('w-full h-4 -my-2', isOver && 'bg-red-600')} ref={ref}>
-                    <div className="" />
+            <div ref={taskDropRef}>
+                <div className="card-title flex justify-between">
+                    <ColumnName id={id} name={name} projectId={project} />
+                    <Menu button={<button className="btn m-1" type="button"><FaEllipsisVertical /></button>}>
+                        <Menu.Item>
+                            <ColumnDelete id={id} projectId={project} />
+                        </Menu.Item>
+                    </Menu>
                 </div>
+                <div className={cn('transition-all h-0', { 'h-12 bg-green-500': isOver })} />
+            </div>
 
+            <div className="flex flex-col p-1">
                 {isLoading ? <span className="loading loading-bars loading-lg" /> : null}
                 {tasks.map((task) => (
                     <Task key={task.id} task={task} />
                 ))}
             </div>
+
             <div className="card-actions p-1 flex justify-end">
                 <ColumnTaskCreate columnId={id} />
             </div>
--- features/Task/Task.tsx
+++ features/Task/Task.tsx
@@ -1,10 +1,10 @@
 'use client';
-import { useDrag } from 'react-dnd';
 import { FaEllipsisVertical } from 'react-icons/fa6';
-import { twMerge } from 'tailwind-merge';
 
 import { Button, DateComponent, Menu } from '@/components';
+import { useTaskDrag, useTaskDrop } from '@/hooks';
 import { useTaskUpdate } from '@/requests';
+import { cn } from '@/utils';
 
 import { TaskProps } from './Task.types';
 import TaskDelete from './TaskDelete';
@@ -13,20 +13,12 @@ import TaskDelete from './TaskDelete';
 const Task = ({ task }: TaskProps) => {
     const { trigger: updateTask } = useTaskUpdate({ taskId: task.id, columnId: task.columnId });
 
-    const [{ isDragging }, dragRef] = useDrag({
-        type: 'TASK',
-        item: {
-            task,
-            updateTask,
-        },
-        collect: (m) => ({
-            isDragging: m.isDragging(),
-        }),
-    });
+    const { isDragging, taskDragRef } = useTaskDrag({ task, updateTask });
+    const { isOver, taskDropRef } = useTaskDrop({ columnId: task.columnId });
 
     return (
-        <div className={twMerge('card bg-primary shadow-lg', isDragging && 'opacity-50')} ref={dragRef}>
-            <div className="card-body p-2">
+        <div ref={taskDropRef}>
+            <div className={cn('card p-2 bg-primary shadow-lg', { 'opacity-50': isDragging })} ref={taskDragRef}>
                 <div className="card-title flex justify-between">
                     {task.title}
                     <Menu button={<Button aria-label="user menu" color="ghost" size="sm"><FaEllipsisVertical /></Button>}>
@@ -35,13 +27,16 @@ const Task = ({ task }: TaskProps) => {
                         </Menu.Item>
                     </Menu>
                 </div>
-                <div className="card-body p-1">
+
+                <div className="p-1">
                     {task.description}
                 </div>
+
                 <div className="card-actions">
                     <DateComponent date={task.completeAt} />
                 </div>
             </div>
+            <div className={cn('transition-all h-2', { 'h-12 bg-green-500': isOver })} />
         </div>
     );
 };
