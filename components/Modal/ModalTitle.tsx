a211e0255acdee50ebe656a3be98329ef9ed15f8
Anton <antklim89@mail.ru>
2023-09-08 12:52:23 +0300

    feat: add className prop

--- components/Modal/ModalTitle.tsx
+++ components/Modal/ModalTitle.tsx
@@ -1,12 +1,14 @@
 import { Dialog } from '@headlessui/react';
 import { ReactNode } from 'react';
 
+import { cn } from '@/utils';
 
-const ModalTitle = ({ children }: { children: ReactNode }) => {
+
+const ModalTitle = ({ children, className }: { children: ReactNode, className?: string }) => {
     return (
         <Dialog.Title
             as="h3"
-            className="text-lg font-medium leading-6 text-base-content mb-4"
+            className={cn('text-lg font-medium leading-6 text-base-content mb-4', className)}
         >
             {children}
         </Dialog.Title>
