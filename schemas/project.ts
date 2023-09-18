563faa57161df43305dd6fdef5267600c8892609
Anton <antklim89@mail.ru>
2023-09-18 11:19:01 +0300

    feat: add description

--- schemas/project.ts
+++ schemas/project.ts
@@ -5,6 +5,17 @@ export const projectSchema = z.object({
     id: z.number(),
     name: z.string(),
     owner: z.string(),
+    description: z.string().optional(),
 });
 
+export const projectUpdateSchema = z.object({
+    name: z.string()
+        .min(3)
+        .max(40),
+    description: z.string()
+        .max(500)
+        .optional(),
+});
+
+export type ProjectUpdateType = z.infer<typeof projectUpdateSchema>
 export type ProjectType = z.infer<typeof projectSchema>
