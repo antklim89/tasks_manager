3151ce8549a28ea12fa1d1b2aa556a13d6821a3b
Anton <antklim89@mail.ru>
2023-09-18 13:23:13 +0300

    feat: add description

--- features/DashboardHome/DashboardHome.tsx
+++ features/DashboardHome/DashboardHome.tsx
@@ -14,9 +14,14 @@ const DashboardHome = () => {
             {isLoading ? <span className="loading loading-bars loading-lg" /> : null}
             <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
                 {projects?.map((project) => (
-                    <Link className="card bg-base-100 shadow-xl hover:bg-base-200 transition" href={`/dashboard/${project.id}`} key={project.id}>
+                    <Link
+                        className="card bg-base-200 shadow-xl hover:bg-base-200 transition"
+                        href={`/dashboard/${project.id}`}
+                        key={project.id}
+                    >
                         <div className="card-body">
                             <h2 className="card-title">{project.name}</h2>
+                            <p>{project.description}</p>
                         </div>
                     </Link>
                 ))}
