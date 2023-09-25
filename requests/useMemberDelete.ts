4038387d33ce66450d3efcafe526a35366c2ccb9
Anton <antklim89@mail.ru>
2023-09-25 13:35:29 +0300

    feat: create member delete request

---
+++ requests/useMemberDelete.ts
@@ -0,0 +1,44 @@
+import { toast } from 'react-hot-toast';
+import useSWRMutation, { SWRMutationConfiguration } from 'swr/mutation';
+
+import { MemberType } from '@/schemas';
+import { getBrowserClient } from '@/supabase/browser';
+
+import { FetchMembersKey } from './keys';
+
+
+const TOAST_ID = 'MEMBER_DELETE';
+
+type Options = SWRMutationConfiguration<void, Error, FetchMembersKey, void>;
+
+export function useMemberDelete({ projectId, memberId }: { projectId: number, memberId: number }, options?: Options) {
+    return useSWRMutation<void, Error, FetchMembersKey, void>(
+        ['MEMBERS', { projectId }],
+
+        async () => {
+            const supabase = getBrowserClient();
+            // const user = await getBrowserUser();
+
+            const { error } = await supabase.from('member')
+                .delete()
+                .eq('id', memberId);
+
+            if (error) throw error;
+        },
+        {
+            ...options,
+            revalidate: false,
+            populateCache(_, currentData: MemberType[]) {
+                currentData.filter((member) => member.id !== memberId);
+            },
+            onSuccess(...args) {
+                toast.success('Member deleted succesfully.', { id: TOAST_ID });
+                options?.onSuccess?.(...args);
+            },
+            onError(...args) {
+                toast.error('Failed to delete a member. Try again later.', { id: TOAST_ID });
+                options?.onError?.(...args);
+            },
+        },
+    );
+}
