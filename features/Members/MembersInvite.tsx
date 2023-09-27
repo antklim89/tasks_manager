cc57912612b6882f9a5a2b2c6582fb780e6e4208
Anton <antklim89@mail.ru>
2023-09-27 21:29:45 +0300

    feat: update invite function

--- features/Members/MembersInvite.tsx
+++ features/Members/MembersInvite.tsx
@@ -3,10 +3,8 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 import { Button, Input, Modal } from '@/components';
-import { SUPABASE_ANON_KEY } from '@/constants';
 import { useDisclosure } from '@/hooks';
-import { getBrowserUser } from '@/supabase/browser';
-import { Database } from '@/supabase/generated-types';
+import { getBrowserClient } from '@/supabase/browser';
 
 
 const MembersInvite = ({ projectId }: { projectId: number }) => {
@@ -19,23 +17,9 @@ const MembersInvite = ({ projectId }: { projectId: number }) => {
         resolver: zodResolver(z.object({ email: z.string().email() })),
     });
 
-    const handleInvite = handleSubmit(async (data) => {
-        // TODO: create edge function
-        const user = await getBrowserUser();
-        await fetch('https://owmyjpfdbksekhxmknmw.supabase.co/rest/v1/members', {
-            method: 'POST',
-            headers: {
-                'apikey': `${SUPABASE_ANON_KEY}`,
-                'Authorization': `Bearer ${SUPABASE_ANON_KEY}`,
-                'Content-Type': 'application/json',
-                'Prefer': 'return=minimal',
-            },
-            body: JSON.stringify({
-                role: 'member',
-                projectId,
-                userId: user.id,
-                email: data.email,
-            } satisfies Database['public']['Tables']['members']['Insert']),
+    const handleInvite = handleSubmit(async ({ email }) => {
+        await getBrowserClient().functions.invoke('invite', {
+            body: { email, projectId },
         });
     });
 
--- supabase/functions/invite/index.ts
+++ supabase/functions/invite/index.ts
@@ -1,30 +1,40 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
+import * as postgres from 'https://deno.land/x/postgres@v0.14.2/mod.ts';
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.36.0';
 
 import type { Database } from '../../../supabase-types-generated.ts';
 import { cors } from '../cors.ts';
 
 
+const databaseUrl = Deno.env.get('SUPABASE_DB_URL');
+const pool = new postgres.Pool(databaseUrl, 3, true);
+
+
 serve(async (req: Request) => {
     if (req.method === 'OPTIONS') return new Response('ok', { headers: cors });
-    // const { projectId, email } = await req.json();
+    const connection = await pool.connect();
+    const { projectId, email } = await req.json();
 
     const supabaseClient = createClient<Database>(
         Deno.env.get('SUPABASE_URL') ?? '',
-        Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+        Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '',
         { global: { headers: { Authorization: req.headers.get('Authorization') ?? '' } } },
     );
 
-    // await supabaseClient.from('member')
-    //     .insert({
-    //         role: 'member',
-    //         projectId,
-    //         userId: user.id,
-    //         email,
-    //     });
+    const { rows: [user] } = await connection.queryObject(`SELECT id FROM auth.users au WHERE au.email = '${email}'`);
+
+    if (!user) return new Response('User with this email not found', { headers: cors, status: 404 });
+
+    await supabaseClient.from('members')
+        .insert({
+            role: 'member',
+            projectId,
+            userId: user.id,
+            email,
+        });
 
     return new Response(
-        '{}',
+        'ok',
         { headers: { 'Content-Type': 'application/json', ...cors } },
     );
 });
