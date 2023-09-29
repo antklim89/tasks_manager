c2950fee75eecc622d51427ee31ea23012b8029f
Anton <antklim89@mail.ru>
2023-09-29 11:43:35 +0300

    style: fix loading

--- features/Members/Members.tsx
+++ features/Members/Members.tsx
@@ -8,14 +8,9 @@ import MembersItem from './MembersItem';
 const Members = () => {
     const { data: members = [], isLoading } = useMembersFetch();
 
-    if (isLoading) return (
-        <>
-            <span className="skeleton h-10 mb-1" />
-            <span className="skeleton h-10 mb-1" />
-        </>
-    );
+
     return (
-        <div className="card container p-2 border border-base-200 max-w-3xl mx-auto ">
+        <div className="card container p-2 border border-base-200 max-w-3xl mx-auto">
             <div className="card-title justify-center">
                 <h3>Members</h3>
             </div>
@@ -30,9 +25,17 @@ const Members = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {members.map((member) => (
-                            <MembersItem key={member.id} member={member} />
-                        )) }
+                        {isLoading
+                            ? [1, 2, 3].map((i) => (
+                                <tr key={i}>
+                                    <td className="skeleton h-10 w-6 mb-1" />
+                                    <td className="skeleton h-10 w-6 mb-1" />
+                                    <td className="skeleton h-10 w-6 mb-1" />
+                                </tr>
+                            ))
+                            : members.map((member) => (
+                                <MembersItem key={member.id} member={member} />
+                            )) }
                     </tbody>
                 </table>
             </div>
