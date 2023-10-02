7b2b1ba40c08cf5c09cfc8b55c3a6b2161af0251
Anton <antklim89@mail.ru>
2023-10-02 14:12:16 +0300

    feat: add NotFoundPage

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+
+const NotFoundPage = () => {
+    return (
+        <div className="h-full flex justify-center items-center text-2xl">
+            404 | Page not found!
+        </div>
+    );
+};
+
+export default NotFoundPage;
