5b0b73b1aa8773a70b81c97f51b17e87cb078582
Anton <antklim89@mail.ru>
2023-10-12 21:07:39 +0300

    feat: check user

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,17 @@
+import { notFound } from 'next/navigation';
+
 import { Profile } from '@/features';
+import { getServerUser } from '@/supabase/server';
 
 
 export const metadata = {
     title: 'Profile',
 };
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+    const user = await getServerUser().catch(() => null);
 
+    if (!user) return notFound();
     return (
         <Profile />
     );
