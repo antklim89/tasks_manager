2c98403747104bebf5ee42e88b23f6eb723bfe47
Anton <antklim89@mail.ru>
2023-10-12 16:35:28 +0300

    feat: create profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,15 @@
+
+export const metadata = {
+    title: 'Profile',
+};
+
+const DashboardPage = () => {
+
+    return (
+        <div>
+            PROFILE
+        </div>
+    );
+};
+
+export default DashboardPage;
