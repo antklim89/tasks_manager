147e3debe0edafce0a3a98c9303c2e3e46b108d7
Anton <antklim89@mail.ru>
2023-10-18 16:00:31 +0300

    style: update History

--- features/History/History.tsx
+++ features/History/History.tsx
@@ -1,16 +1,21 @@
 'use client';
+import format from 'date-fns/esm/format';
+
 import { useHistoryFetch } from '@/requests';
 
 
 const History = () => {
-    const { data: history = [], isLoading } = useHistoryFetch();
+    const { data: history = [] } = useHistoryFetch();
 
     return (
-        <div>
+        <div className="p-4">
             {history.map((historyItem) => (
-                <div key={historyItem.id}>
-                    {historyItem.table}
-                </div>
+                <p className="p-2 even:bg-primary" key={historyItem.id}>
+                    <span className="inline-block pr-4 min-w-[250px]">
+                        {format(new Date(historyItem.createdAt), 'dd-MMM-yyy hh:mm:ss')}
+                    </span>
+                    <span>{historyItem.table}</span>
+                </p>
             ))}
         </div>
     );
