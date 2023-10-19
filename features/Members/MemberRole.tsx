e3b9120a20fe5e1046dc7ad90af16f5dbeca7604
Anton <antklim89@mail.ru>
2023-10-19 16:10:01 +0300

    feat: add confirm dialog

--- features/Members/MemberRole.tsx
+++ features/Members/MemberRole.tsx
@@ -1,12 +1,13 @@
-import { MouseEventHandler } from 'react';
+import { Fragment, MouseEventHandler } from 'react';
 
-import { Button, Menu } from '@/components';
-import { useMember } from '@/hooks';
+import { Button, Confirm, Menu } from '@/components';
+import { useDisclosure, useMember } from '@/hooks';
 import { useMemberUpdate } from '@/requests';
 import { MemberType, MemberUpdateType, updateRoles } from '@/schemas';
 
 
 const MemberRole = ({ member, members }: { member: MemberType, members: MemberType[] }) => {
+    const { isOpen, close, open } = useDisclosure();
     const { trigger: updateMember, isMutating } = useMemberUpdate({ memberId: member.id });
     const { isAdmin, member: userMember } = useMember();
     const isYou = userMember?.id === member.id;
@@ -34,16 +35,25 @@ const MemberRole = ({ member, members }: { member: MemberType, members: MemberTy
             {updateRoles
                 .filter((role) => role !== member.role)
                 .map((role) => (
-                    <Button
-                        className="btn-xs sm:btn-sm"
-                        color="ghost"
-                        isLoading={isMutating}
-                        key={role}
-                        name={role}
-                        onClick={handleSelectRole}
-                    >
-                        {role}
-                    </Button>
+                    <Fragment key={role}>
+                        <Confirm
+                            isLoading={isMutating}
+                            isOpen={isOpen}
+                            name={role}
+                            text="Are you sure you want to change your role. You can't change it back."
+                            onClose={close}
+                            onConfirm={handleSelectRole}
+                        />
+                        <Button
+                            className="btn-xs sm:btn-sm"
+                            color="ghost"
+                            isLoading={isMutating}
+                            name={role}
+                            onClick={((userMember?.id === member.id) && (member.role === 'admin')) ? open : handleSelectRole}
+                        >
+                            {role}
+                        </Button>
+                    </Fragment>
                 ))}
         </Menu>
     );
