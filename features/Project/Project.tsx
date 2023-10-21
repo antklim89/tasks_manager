e5b6fe5663c07b512bed399ddcb718db8d0df6de
Anton <antklim89@mail.ru>
2023-10-21 15:07:01 +0300

    refactor: create hook

--- features/Project/Project.tsx
+++ features/Project/Project.tsx
@@ -2,78 +2,24 @@
 
 import {
     DndContext,
-    DragEndEvent,
-    KeyboardSensor,
-    MouseSensor, TouchSensor,
     pointerWithin,
-    useSensor,
-    useSensors,
 } from '@dnd-kit/core';
-import { useSWRConfig } from 'swr';
 
 import { Button } from '@/components';
 import Column from '@/features/Column';
 import { useMember } from '@/hooks';
-import { columnUpdate, taskUpdate, useColumnCreate, useColumnsFetch } from '@/request-hooks';
-import { ColumnType, TaskType } from '@/schemas';
-import { TasgDragData, TaskDropData } from '@/types';
+import { useColumnCreate, useColumnsFetch } from '@/request-hooks';
+import { ColumnType } from '@/schemas';
+
+import { useProject } from './Project.use';
 
 
 const Project = ({ columns: defaultColumns }: { columns: ColumnType[] }) => {
     const { trigger: createColumn, isMutating } = useColumnCreate();
     const { data: columns = [], isLoading } = useColumnsFetch({ defaultValue: defaultColumns });
-    const { mutate } = useSWRConfig();
     const { isAdmin } = useMember();
+    const { sensors, handleDrop } = useProject();
 
-    const handleCreateColumn = () => {
-        createColumn();
-    };
-
-    const mouseSensor = useSensor(MouseSensor);
-    const touchSensor = useSensor(TouchSensor);
-    const keyboardSensor = useSensor(KeyboardSensor);
-    const sensors = useSensors(mouseSensor, touchSensor, keyboardSensor);
-
-    const handleDrop = ({ active, over }: DragEndEvent) => {
-        const activeData = active.data.current as TasgDragData | undefined;
-        const overData = over?.data.current as TaskDropData | undefined;
-        if (!overData || !activeData) return;
-        if (overData.task?.id === activeData.task.id) return;
-
-        if (overData.columnId === activeData.columnId) {
-
-            mutate<TaskType[]>(['TASKS', { columnId: activeData.columnId }], (currentTasks) => {
-                if (!currentTasks) return currentTasks;
-                const newTasks = currentTasks.toSpliced((overData.index ?? -1) + 1, 0, activeData.task);
-
-                if ((overData.index || -1) < activeData.index) newTasks.splice(activeData.index + 1, 1);
-                else newTasks.splice(activeData.index, 1);
-
-                columnUpdate(overData.columnId, { taskOrder: newTasks.map((i) => i.id) });
-                return newTasks;
-            }, { revalidate: false });
-
-            return;
-        }
-
-        taskUpdate(activeData.task.id, { columnId: overData.columnId });
-
-        mutate<TaskType[]>(['TASKS', { columnId: overData.columnId }], (currentTasks) => {
-            if (!currentTasks) return currentTasks;
-            const newTask = { ...activeData.task, columnId: overData.columnId };
-            const newTasks = currentTasks.toSpliced((overData.index ?? -1) + 1, 0, newTask);
-
-            columnUpdate(overData.columnId, { taskOrder: newTasks.map((i) => i.id) });
-            return newTasks;
-        }, { revalidate: false });
-
-        mutate<TaskType[]>(['TASKS', { columnId: activeData.columnId }], (currentTasks) => {
-            const newTasks = currentTasks?.filter((task) => task.id !== activeData.task.id);
-
-            if (newTasks) columnUpdate(activeData.columnId, { taskOrder: newTasks.map((i) => i.id) });
-            return newTasks;
-        }, { revalidate: false });
-    };
 
     if (isLoading) return <span className="loading loading-bars loading-lg" />;
     return (
@@ -87,7 +33,7 @@ const Project = ({ columns: defaultColumns }: { columns: ColumnType[] }) => {
                     <Column key={column.id} {...column} />
                 ))}
                 {isAdmin
-                    ? <Button isLoading={isMutating} onClick={handleCreateColumn}>Create new column</Button>
+                    ? <Button isLoading={isMutating} onClick={() => createColumn()}>Create new column</Button>
                     : null}
             </div>
         </DndContext>
---
+++ features/Project/Project.use.ts
@@ -0,0 +1,65 @@
+import {
+    DragEndEvent,
+    KeyboardSensor,
+    MouseSensor, TouchSensor,
+    useSensor,
+    useSensors,
+} from '@dnd-kit/core';
+import { useSWRConfig } from 'swr';
+
+import { columnUpdate, taskUpdate } from '@/request-hooks';
+import { TaskType } from '@/schemas';
+import { TasgDragData, TaskDropData } from '@/types';
+
+
+export function useProject() {
+    const { mutate } = useSWRConfig();
+
+    const mouseSensor = useSensor(MouseSensor);
+    const touchSensor = useSensor(TouchSensor);
+    const keyboardSensor = useSensor(KeyboardSensor);
+    const sensors = useSensors(mouseSensor, touchSensor, keyboardSensor);
+
+    const handleDrop = ({ active, over }: DragEndEvent) => {
+        const activeData = active.data.current as TasgDragData | undefined;
+        const overData = over?.data.current as TaskDropData | undefined;
+        if (!overData || !activeData) return;
+        if (overData.task?.id === activeData.task.id) return;
+
+        if (overData.columnId === activeData.columnId) {
+
+            mutate<TaskType[]>(['TASKS', { columnId: activeData.columnId }], (currentTasks) => {
+                if (!currentTasks) return currentTasks;
+                const newTasks = currentTasks.toSpliced((overData.index ?? -1) + 1, 0, activeData.task);
+
+                if ((overData.index || -1) < activeData.index) newTasks.splice(activeData.index + 1, 1);
+                else newTasks.splice(activeData.index, 1);
+
+                columnUpdate(overData.columnId, { taskOrder: newTasks.map((i) => i.id) });
+                return newTasks;
+            }, { revalidate: false });
+
+            return;
+        }
+
+        taskUpdate(activeData.task.id, { columnId: overData.columnId });
+
+        mutate<TaskType[]>(['TASKS', { columnId: overData.columnId }], (currentTasks) => {
+            if (!currentTasks) return currentTasks;
+            const newTask = { ...activeData.task, columnId: overData.columnId };
+            const newTasks = currentTasks.toSpliced((overData.index ?? -1) + 1, 0, newTask);
+
+            columnUpdate(overData.columnId, { taskOrder: newTasks.map((i) => i.id) });
+            return newTasks;
+        }, { revalidate: false });
+
+        mutate<TaskType[]>(['TASKS', { columnId: activeData.columnId }], (currentTasks) => {
+            const newTasks = currentTasks?.filter((task) => task.id !== activeData.task.id);
+
+            if (newTasks) columnUpdate(activeData.columnId, { taskOrder: newTasks.map((i) => i.id) });
+            return newTasks;
+        }, { revalidate: false });
+    };
+
+    return { handleDrop, sensors };
+}
