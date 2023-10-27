90c06e0fe2c3c6dc803498520a88c873aabb1781
Anton <antklim89@mail.ru>
2023-10-27 15:13:24 +0300

    feat: add Tables select

--- features/History/HistoryPanel.tsx
+++ features/History/HistoryPanel.tsx
@@ -1,31 +1,42 @@
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
 
-import { DatePicker, Input } from '@/components';
+import { Button, DatePicker, Input, Menu } from '@/components';
+import { HistoryTables, tables } from '@/schemas';
 
 import { HistoryPanelProps } from './History.types';
 
 
 const HistoryPanel = ({ onStartDateChange }: HistoryPanelProps) => {
     const [startDate, setStartDate] = useState<Date|null>(null);
+    const [table, setTable] = useState<HistoryTables|null>(null);
 
     const handleStartDateChange = (date?: Date | null): void => {
         setStartDate(date || null);
         onStartDateChange(date || null);
     };
 
+    const handleTableChange: MouseEventHandler<HTMLButtonElement> = (e) => {
+        setTable(e.currentTarget.name as HistoryTables);
+    };
     return (
-        <div>
+        <div className="flex justify-start items-center gap-2">
             <DatePicker
+                className="w-auto"
                 customInput={(
                     <Input
-                        className="w-full"
-                        label="Start at"
                         reset={handleStartDateChange}
                     />
                 )}
-                value={startDate}
+                placeholderText="Filter by date"
+                selected={startDate}
                 onChange={handleStartDateChange}
             />
+            <Menu button={<Button>{table || 'Filter by table'}</Button>}>
+                <Button onClick={handleTableChange}>ALL</Button>
+                {tables.map((t) => (
+                    <Button key={t} name={t} onClick={handleTableChange}>{t}</Button>
+                ))}
+            </Menu>
         </div>
     );
 };
