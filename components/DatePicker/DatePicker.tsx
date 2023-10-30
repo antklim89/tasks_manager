9cde05dcbd892ed45efcb8b968278ef296de63b4
Anton <antklim89@mail.ru>
2023-10-30 14:13:12 +0300

    style: remove w-full

--- components/DatePicker/DatePicker.tsx
+++ components/DatePicker/DatePicker.tsx
@@ -11,12 +11,10 @@ const DatePicker = ({ onChange, customInput, ...props }: DatePickerProps) => {
     return (
         <ReactDatePicker
             showTimeSelect
-            className="w-full"
             customInput={customInput}
             dateFormat={formatDateString}
             timeFormat="HH:mm"
             timeIntervals={10}
-            wrapperClassName="w-full"
             onChange={onChange}
             {...props}
         />
