9fe2ab60343e0c84f70e7bd5bd1ae3e10882d607
Anton <antklim89@mail.ru>
2023-11-01 15:11:15 +0300

    feat: add historyCreate request

--- requests/index.ts
+++ requests/index.ts
@@ -1,6 +1,7 @@
 // generate export all files
 
 export * from './columnsFetch';
+export * from './historyCreate';
 export * from './memberFetch';
 export * from './membersFetch';
 export * from './profileFetch';
