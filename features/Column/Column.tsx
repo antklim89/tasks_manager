fe804df8c5f4b0b9834342e022902875869e46a5
Anton <antklim89@mail.ru>
2023-11-07 15:41:40 +0300

    feat: add ColumnContext

--- features/Column/Column.tsx
+++ features/Column/Column.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { createContext } from 'react';
 import { FaEllipsisVertical } from 'react-icons/fa6';
 
 import { Menu } from '@/components';
@@ -13,6 +14,8 @@ import ColumnName from './ColumnName';
 import ColumnTaskCreate from './ColumnTaskCreate';
 
 
+export const ColumnContext = createContext<ColumnType|null>(null);
+
 const Column = ({ column, defaultTasks }: { column: ColumnType, defaultTasks?: TaskType[] }) => {
     const { data: tasks = [], isLoading } = useTasksFetch({
         columnId: column.id,
@@ -21,36 +24,35 @@ const Column = ({ column, defaultTasks }: { column: ColumnType, defaultTasks?: T
     const { isAdmin, isAdminOrUser: isAdminOrMember } = useMember();
 
     return (
-        <div
-            className="card w-96 bg-base-200 shadow-xl"
-        >
-            <div className="flex items-start">
-                <ColumnName id={column.id} name={column.name} />
-                {isAdminOrMember ? <ColumnTaskCreate column={column} /> : null}
-                {isAdmin
-                    ? (
-                        <Menu button={<button className="btn" type="button"><FaEllipsisVertical /></button>}>
-                            <ColumnDelete id={column.id} />
-                        </Menu>
-                    )
-                    : null}
-            </div>
+        <ColumnContext.Provider value={column}>
+            <div
+                className="card w-96 bg-base-200 shadow-xl"
+            >
+                <div className="flex items-start">
+                    <ColumnName id={column.id} name={column.name} />
+                    {isAdminOrMember ? <ColumnTaskCreate column={column} /> : null}
+                    {isAdmin
+                        ? (
+                            <Menu button={<button className="btn" type="button"><FaEllipsisVertical /></button>}>
+                                <ColumnDelete id={column.id} />
+                            </Menu>
+                        )
+                        : null}
+                </div>
 
-            <div className="flex flex-col p-1 relative">
-                <TaskDrop
-                    columnId={column.id}
-                    index={-1}
-                />
-                {isLoading ? <span className="loading loading-bars loading-lg" /> : null}
-                {tasks.map((task, index) => (
-                    <Task
-                        index={index}
-                        key={task.id}
-                        task={task}
-                    />
-                ))}
+                <div className="flex flex-col p-1 relative">
+                    <TaskDrop index={-1} />
+                    {isLoading ? <span className="loading loading-bars loading-lg" /> : null}
+                    {tasks.map((task, index) => (
+                        <Task
+                            index={index}
+                            key={task.id}
+                            task={task}
+                        />
+                    ))}
+                </div>
             </div>
-        </div>
+        </ColumnContext.Provider>
     );
 };
 
