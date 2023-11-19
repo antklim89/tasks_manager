095e665947c5d18ad6bfd0a8a03cd8327c755360
Anton <antklim89@mail.ru>
2023-11-19 15:10:49 +0300

    feat: change menu position on scroll

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef, useState } from 'react';
+
 import { cn } from '@/utils';
 
 import { MenuProps } from './Menu.types';
@@ -5,10 +7,36 @@ import MenuItem from './MenuItem';
 
 
 const Menu = ({ button, children, className, ...props }: MenuProps) => {
+    const [isLeft, setIsLeft] = useState(false);
+    const itemRef = useRef<HTMLUListElement>(null);
+    const menuRef = useRef<HTMLDivElement>(null);
+    
+    useEffect(() => {
+        const element = document.querySelector('.overflow-x-scroll');
+        let timeoutId: NodeJS.Timeout;
+        const itemWidth = itemRef.current?.getBoundingClientRect().width;
+        
+        const listener = () => {
+            if (timeoutId) clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                const menuLeft = menuRef.current?.getBoundingClientRect().left;
+                if (!menuLeft || !itemWidth) return;
+                
+                if (menuLeft - itemWidth < 0) setIsLeft(true);
+                else setIsLeft(false);
+            }, 100);
+        };
+        element?.addEventListener('scroll', listener);
+        return () => {
+            element?.removeEventListener('scroll', listener);
+        };
+    }, []);
+    
+
     return (
-        <div className={cn('dropdown dropdown-bottom dropdown-end', className)} {...props}>
+        <div className={cn('dropdown dropdown-bottom', { 'dropdown-end': !isLeft }, className)} ref={menuRef} {...props}>
             {button}
-            <ul className="dropdown-content menu z-[1] p-2 shadow bg-base-200 rounded-box w-52">
+            <ul className="dropdown-content menu z-[1] p-2 shadow bg-base-200 rounded-box w-52" ref={itemRef}>
                 {children}
             </ul>
         </div>
