cd1ddee2c6dc413e0b71fcaf34acd6db3c74f618
Anton <antklim89@mail.ru>
2023-11-20 14:12:41 +0300

    feat: add date format

--- utils/formatHistoryData.tsx
+++ utils/formatHistoryData.tsx
@@ -1,3 +1,9 @@
+import { formatDate } from './formatDate';
+
+
+const isISODate = (date: string) => 
+    /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/.test(date);
+
 export function formatHistoryData<T extends Record<string, unknown>>({
     data: obj, oldData, fields, startText = '', endText = '.',
 }: {
@@ -10,9 +16,10 @@ export function formatHistoryData<T extends Record<string, unknown>>({
     const text = Object.entries(obj)
         .filter(([key]) => fields ? fields.includes(key) : true)
         .filter(([key, val]) => oldData ? oldData[key] !== val : true)
-        .map(([k, v]) => `${k}: "${v}"`)
+        .map(([k, v]) => `${k}: "${typeof v === 'string' && isISODate(v) ? formatDate(v) : v}"`)
         .join(', ');
 
     if (text.length === 0) return '';
     return `${startText}${text}${endText}`;
 }
+
