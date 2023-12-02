97afe93e7f8b9697e0df43d3a0a6acfc94563b25
Anton <antklim89@mail.ru>
2023-12-02 14:20:49 +0300

    feat: remove DateComponent

--- components/DateComponent/DateComponent.types.ts
+++
@@ -1,7 +0,0 @@
-import { HTMLAttributes } from 'react';
-
-
-export interface DateComponentProps extends HTMLAttributes<HTMLInputElement> {
-    date?: string | Date | number | null
-    format?: string
-}
--- components/index.ts
+++ components/index.ts
@@ -3,7 +3,6 @@
 export { default as Alert } from './Alert';
 export { default as Button } from './Button';
 export { default as Confirm } from './Confirm';
-export { default as DateComponent } from './DateComponent';
 export { default as DatePicker } from './DatePicker';
 export { default as Input } from './Input';
 export { default as Menu } from './Menu';
