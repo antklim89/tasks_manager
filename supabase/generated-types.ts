866cd01bd1b84f0f8cb41b2c19697eaa0f7d942b
Anton <antklim89@mail.ru>
2023-12-02 20:31:41 +0300

    updated

--- supabase/generated-types.ts
+++ supabase/generated-types.ts
@@ -32,6 +32,7 @@ export interface Database {
           {
             foreignKeyName: "columns_projectId_fkey"
             columns: ["projectId"]
+            isOneToOne: false
             referencedRelation: "projects"
             referencedColumns: ["id"]
           }
@@ -63,12 +64,14 @@ export interface Database {
           {
             foreignKeyName: "history_projectId_fkey"
             columns: ["projectId"]
+            isOneToOne: false
             referencedRelation: "projects"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "history_userId_fkey"
             columns: ["userId"]
+            isOneToOne: false
             referencedRelation: "profiles"
             referencedColumns: ["id"]
           }
@@ -103,12 +106,14 @@ export interface Database {
           {
             foreignKeyName: "members_projectId_fkey"
             columns: ["projectId"]
+            isOneToOne: false
             referencedRelation: "projects"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "members_userId_fkey"
             columns: ["userId"]
+            isOneToOne: false
             referencedRelation: "profiles"
             referencedColumns: ["id"]
           }
@@ -143,6 +148,7 @@ export interface Database {
           {
             foreignKeyName: "profiles_id_fkey"
             columns: ["id"]
+            isOneToOne: true
             referencedRelation: "users"
             referencedColumns: ["id"]
           }
@@ -168,33 +174,42 @@ export interface Database {
       }
       tasks: {
         Row: {
+          color: string | null
           columnId: number
           completeAt: string | null
           createdAt: string
+          creator: string | null
           description: string
           id: number
+          priority: Database["public"]["Enums"]["priorities"] | null
           projectId: number
           shouldByIn: number | null
           startAt: string | null
           title: string
         }
         Insert: {
+          color?: string | null
           columnId: number
           completeAt?: string | null
           createdAt?: string
+          creator?: string | null
           description?: string
           id?: number
+          priority?: Database["public"]["Enums"]["priorities"] | null
           projectId: number
           shouldByIn?: number | null
           startAt?: string | null
           title: string
         }
         Update: {
+          color?: string | null
           columnId?: number
           completeAt?: string | null
           createdAt?: string
+          creator?: string | null
           description?: string
           id?: number
+          priority?: Database["public"]["Enums"]["priorities"] | null
           projectId?: number
           shouldByIn?: number | null
           startAt?: string | null
@@ -204,18 +219,28 @@ export interface Database {
           {
             foreignKeyName: "tasks_columnId_fkey"
             columns: ["columnId"]
+            isOneToOne: false
             referencedRelation: "columns"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "tasks_creator_fkey"
+            columns: ["creator"]
+            isOneToOne: false
+            referencedRelation: "profiles"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "tasks_projectId_fkey"
             columns: ["projectId"]
+            isOneToOne: false
             referencedRelation: "projects"
             referencedColumns: ["id"]
           },
           {
             foreignKeyName: "tasks_shouldByIn_fkey"
             columns: ["shouldByIn"]
+            isOneToOne: false
             referencedRelation: "columns"
             referencedColumns: ["id"]
           }
@@ -278,6 +303,7 @@ export interface Database {
     }
     Enums: {
       operations: "INSERT" | "UPDATE" | "DELETE"
+      priorities: "very low" | "low" | "medium" | "high" | "very high"
       roles: "admin" | "user" | "guest" | "invited"
       tables: "tasks" | "projects" | "columns" | "members"
     }
@@ -286,3 +312,83 @@ export interface Database {
     }
   }
 }
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
