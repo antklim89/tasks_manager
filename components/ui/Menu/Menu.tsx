77150e2fb67a8c1c2c58d56ca7bc3db9624b2650
Anton <antklim89@mail.ru>
2023-12-03 16:30:13 +0300

    style: add listClassName

--- components/ui/Menu/Menu.tsx
+++ components/ui/Menu/Menu.tsx
@@ -7,7 +7,7 @@ import { MenuProps } from './Menu.types';
 import MenuItem from './MenuItem';
 
 
-const Menu = ({ button, children, className, ...props }: MenuProps) => {
+const Menu = ({ button, children, className, listClassName, ...props }: MenuProps) => {
     const [isLeft, setIsLeft] = useState(false);
     const itemRef = useRef<HTMLUListElement>(null);
     const menuRef = useRef<HTMLDivElement>(null);
@@ -37,7 +37,7 @@ const Menu = ({ button, children, className, ...props }: MenuProps) => {
     return (
         <div className={cn('dropdown dropdown-bottom', { 'dropdown-end': !isLeft }, className)} ref={menuRef} {...props}>
             {button}
-            <ul className="dropdown-content menu z-[1] p-2 shadow bg-base-200 rounded-box w-52" ref={itemRef}>
+            <ul className={cn('dropdown-content menu z-[1] p-2 shadow bg-base-200 rounded-box w-52', listClassName)} ref={itemRef}>
                 {children}
             </ul>
         </div>
--- components/ui/Menu/Menu.types.ts
+++ components/ui/Menu/Menu.types.ts
@@ -4,6 +4,7 @@ import { ReactNode, HTMLAttributes } from 'react';
 export interface MenuProps extends HTMLAttributes<HTMLDivElement> {
     children: ReactNode
     button: ReactNode
+    listClassName?: string
 }
 
 export interface MenuItemProps extends HTMLAttributes<HTMLDivElement> {
