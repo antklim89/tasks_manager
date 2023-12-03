374f4216bd175901b44be75328ecf943d2943a81
Anton <antklim89@mail.ru>
2023-12-03 16:30:38 +0300

    feat: add colors

--- constants.ts
+++ constants.ts
@@ -9,4 +9,4 @@ export const SUPABASE_URL = z.string().parse(process.env.NEXT_PUBLIC_SUPABASE_UR
 
 
 export const priorities = ['low', 'medium', 'high', 'very high'] as const;
-export const colors = ['FF8811', '24251E', '0A2472', '392F5A', '3C1518'] as const;
+export const colors = ['F7FFF7', '24251E', '0A2472', '392F5A', '3C1518', '60935D', 'FAFF81'] as const;
