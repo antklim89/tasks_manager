2c611f4abf20f442244fadd5ce6baafb40954f13
Anton <antklim89@mail.ru>
2023-12-23 14:30:34 +0300

    style: improve adaptivity

--- components/features/History/HistoryItem.tsx
+++ components/features/History/HistoryItem.tsx
@@ -3,9 +3,16 @@ import { formatDate } from '@/utils';
 
 
 const HistoryItem = ({ historyItem }: { historyItem: HistoryType }) => {
+    const date = formatDate(historyItem.createdAt, 'dd MMM yyyy');
+    const time = formatDate(historyItem.createdAt, 'HH:mm');
+
     return (
-        <p className="flex gap-4 p-2 even:bg-primary" key={historyItem.id}>
-            <span>{formatDate(historyItem.createdAt)}</span>
+        <p className="flex items-center text-sm gap-2 p-1 sm:text-md sm:gap-4 sm:p-4 even:bg-primary" key={historyItem.id}>
+            <span className="flex flex-col sm:flex-row gap-2">
+                <span className='whitespace-nowrap'>{date}</span>
+                <span className='whitespace-nowrap'>{time}</span>
+            </span>
+            <span>-</span>
             <span>{historyItem.body}</span>
         </p>
     );
