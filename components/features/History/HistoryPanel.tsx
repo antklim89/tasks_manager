e11a7aca1e4c154cbee684ca09a5ccdb1251926d
Anton <antklim89@mail.ru>
2023-12-23 18:13:49 +0300

    feat: add history search

--- components/features/History/HistoryPanel.tsx
+++ components/features/History/HistoryPanel.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { ChangeEventHandler, useState } from 'react';
 
 import { DatePicker, Input } from '@/components';
+import { useSearchParamsState } from '@/hooks';
 
 import { HistoryPanelProps } from './History.types';
 
 
 const HistoryPanel = ({ onStartDateChange, defaultStartDate }: HistoryPanelProps) => {
     const [startDate, setStartDate] = useState<string|undefined>(defaultStartDate);
+    const [search, setSearch] = useSearchParamsState('search', { defer: 700 });
 
     const handleStartDateChange = (date?: Date | null): void => {
         setStartDate(date?.toISOString());
         onStartDateChange(date?.toISOString());
+    };
+
+
+    const handleSearchChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setSearch(e.target.value);
     };
 
     return (
@@ -22,6 +29,12 @@ const HistoryPanel = ({ onStartDateChange, defaultStartDate }: HistoryPanelProps
                 placeholderText="Filter by date"
                 selected={startDate ? new Date(startDate) : null}
                 onChange={handleStartDateChange}
+            />
+            <Input
+                placeholder='Search'
+                reset={() => setSearch('')}
+                value={search ?? ''}
+                onChange={handleSearchChange}
             />
         </div>
     );
