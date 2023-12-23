1f70c7cfc046bcaa2392f4108a68c35271c1c97b
Anton <antklim89@mail.ru>
2023-12-23 18:13:29 +0300

    feat: feat: add debounce to useSearchParamsState

--- hooks/useSearchParamsState.ts
+++ hooks/useSearchParamsState.ts
@@ -1,29 +1,38 @@
 import { useRouter, useSearchParams } from 'next/navigation';
+import { useRef, useState } from 'react';
 import { z, Schema } from 'zod';
 
 
 export function useSearchParamsState(
     name: string,
+    options?: { defer?: number },
 ): readonly [string | undefined, (newState?: string | null) => void]
 
 export function useSearchParamsState<S extends Schema>(
     name: string,
-    schema: S,
+    options?:{ defer?: number, schema: S, },
 ): readonly [z.infer<S> | undefined, (newState?: string | null) => void]
 
 export function useSearchParamsState<S extends Schema>(
     name: string,
-    schema?: S,
+    { schema, defer }: { defer?: number, schema?: S, } = {},
 ): readonly [z.infer<S> | undefined, (newState?: string | null) => void] {
-    const { replace } = useRouter();
-    const state = (schema ? schema.optional().catch(undefined) : z.string().optional().catch(undefined))
+    const queryState = (schema ? schema.optional().catch(undefined) : z.string().optional().catch(undefined))
         .parse(useSearchParams().get(name));
 
+    const timeoutId = useRef<NodeJS.Timeout|null>(null);
+    const { replace } = useRouter();
+    const [state, setState] = useState(queryState);
+
     const changeState = (newState?: string | null): void => {
-        const searchParams = new URLSearchParams(location.search);
-        if (newState) searchParams.set(name, newState);
-        else searchParams.delete(name);
-        replace(`?${searchParams.toString()}`, {});
+        setState(newState);
+        if (timeoutId.current) clearTimeout(timeoutId.current);
+        timeoutId.current = setTimeout(() => {
+            const searchParams = new URLSearchParams(location.search);
+            if (newState) searchParams.set(name, newState);
+            else searchParams.delete(name);
+            replace(`?${searchParams.toString()}`, {});
+        }, defer);
     };
 
     return [state, changeState] as const;
