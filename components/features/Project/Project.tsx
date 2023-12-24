5cc8f7fbae8cfef43d1b46e3fdc7fe77e92c5823
Anton <antklim89@mail.ru>
2023-12-24 17:52:34 +0300

    feat: make tasks scroll disable if there not overflow

--- components/features/Project/Project.tsx
+++ components/features/Project/Project.tsx
@@ -1,17 +1,37 @@
 'use client';
+import { useEffect, useState } from 'react';
+
 import { Column, TaskDndContext } from '@/components';
 import { ColumnContext } from '@/hooks';
 import { useColumnsFetch } from '@/request-hooks';
+import { cn } from '@/utils';
 
 import ProjectCreateColumn from './ProjectCreateColumn';
 
 
 const Project = () => {
     const { data: columns = [] } = useColumnsFetch();
+    const [ref, setRef] = useState<HTMLDivElement|null>(null);
+    const [isWidthOverflow, setIsWidthOverflow] = useState(false);
+    const [isHeightOverflow, setIsHeightOverflow] = useState(false);
+
+    useEffect(() => {
+        if (!ref) return () => null;
+        const listener = () => {
+            setIsWidthOverflow(ref.scrollWidth > document.body.clientWidth);
+            setIsHeightOverflow(ref.scrollHeight > document.body.clientHeight);
+        };
+        listener();
+        window.addEventListener('resize', listener);
+        return () => window.removeEventListener('resize', listener);
+    }, [ref]);
 
     return (
         <TaskDndContext>
-            <div className="h-full flex flex-grow items-start gap-2 overflow-x-scroll overflow-y-scroll">
+            <div
+                className={cn('flex flex-grow items-start gap-2', { 'overflow-x-scroll': isWidthOverflow, 'overflow-y-scroll': isHeightOverflow })} 
+                ref={setRef}
+            >
                 {columns.map((column) => (
                     <ColumnContext.Provider key={column.id} value={column}>
                         <Column />
