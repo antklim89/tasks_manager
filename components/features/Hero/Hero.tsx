a5baef23cae371b733c30de5e1c461d0a8ced565
Anton <antklim89@mail.ru>
2023-12-24 13:16:19 +0300

    feat: change Hero text

--- components/features/Hero/Hero.tsx
+++ components/features/Hero/Hero.tsx
@@ -4,11 +4,19 @@ import Link from 'next/link';
 
 const Hero = () => {
     return (
-        <div className="hero h-full">
-            <div className="hero-content max-w-7xl flex-col md:flex-row">
+        <div className="container hero h-full mb-8 md:mb-32">
+            <div className="hero-content p-0 max-w-7xl flex-col md:flex-row">
                 <div className="flex-1">
                     <h1 className="text-5xl font-bold">Manage your tasks!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className="py-6">The online task manager app with a kanban
+                        board is a powerful and intuitive tool 
+                        designed to help individuals and teams 
+                        efficiently manage their tasks and projects.
+                        This comprehensive app combines the benefits 
+                        of a task manager with the visual organization of
+                        a kanban board, providing users with a seamless
+                        and flexible task management experience.
+                    </p>
                     <Link className="btn btn-primary" href="/dashboard">Get Started</Link>
                 </div>
                 <div className="flex-1">
