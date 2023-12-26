48e562cda58a4e59d2ba0a6cc98b0c256e8218a9
Anton <antklim89@mail.ru>
2023-12-26 19:57:53 +0300

    style: uptate features layaout

--- components/features/Features/Features.tsx
+++ components/features/Features/Features.tsx
@@ -3,31 +3,32 @@ import Image from 'next/image';
 
 const Features = () => {
     return (
-        <section className='container '>
+        <section className='container'>
             <div className='flex flex-row gap-1 md:gap-4 mb-1 md:mb-4 flex-wrap'>
-                <div className='w-full flex-1 border border-primary rounded-lg p-4 basis-96'>
-                    <div className='mb-8 flex-1'>
-                        <p>
+                <div className='flex flex-col gap-1 md:gap-4 justify-between w-full flex-1 border border-primary rounded-lg p-4 basis-96 items-center'>
+                    <div>
+                        <p className='leading-6 text-justify'>
                             The app also enables collaboration and seamless communication
                             among team members. Team members can leave comments, update
                             task statuses, and even mention specific users to ensure
                             everyone is on the same page.
                         </p>
                     </div>
-                    <div className='flex-1'>
+                    <div>
                         <Image
                             alt="feature"
+                            className='object-cover w-full'
                             height={720}
                             quality={90}
-                            src="/members_page.png"
+                            src="/feature-1.jpg"
                             width={1024}
                         />
                     </div>
                 </div>
 
-                <div className='w-full flex-[2] border border-primary rounded-lg p-4 basis-96 flex'>
-                    <div className='mb-8 flex-[2]'>
-                        <p>
+                <div className='w-full flex-[3] gap-1 md:gap-4 border border-primary rounded-lg p-4 basis-96 flex flex-col sm:flex-row items-center'>
+                    <div className='flex-1'>
+                        <p className='leading-6 text-justify'>
                             In summary, the online task manager app with a kanban board
                             is a robust and user-friendly tool designed to streamline
                             task management, enhance collaboration, and improve productivity.
@@ -40,18 +41,19 @@ const Features = () => {
                     <div className='flex-1'>
                         <Image
                             alt="feature"
-                            height={720}
+                            className='object-cover w-full'
+                            height={640}
                             quality={90}
-                            src="/edit_modal.png"
-                            width={1024}
+                            src="/feature-2.jpg"
+                            width={320}
                         />
                     </div>
                 </div>
             </div>
 
-            <div className='flex flex-col md:flex-row border border-primary rounded-lg p-4'>
-                <div className='mb-8'>
-                    <p>
+            <div className='flex flex-col gap-1 md:gap-4 md:flex-row border border-primary rounded-lg p-4 items-center'>
+                <div className=''>
+                    <p className='leading-6 text-justify'>
                         The app&apos;s kanban board allows users to visually track
                         the progress of their tasks and projects using customizable
                         columns, such as &quot;To Do,&quot; &quot;In Progress,&quot; and &quot;Completed.&quot;
@@ -61,12 +63,13 @@ const Features = () => {
                         and what has been completed.
                     </p>
                 </div>
-                <div>
+                <div className='shrink-1'>
                     <Image
                         alt="feature"
-                        height={720}
+                        className='object-cover h-64'
+                        height={256}
                         quality={90}
-                        src="/project_page.png"
+                        src="/feature-3.jpg"
                         width={1024}
                     />
                 </div>
