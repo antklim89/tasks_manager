d407d98e1f1bfc10a916fa64d8115b8dcbb4bf3c
Anton <antklim89@mail.ru>
2024-01-03 20:03:55 +0300

    feat: add comment delete

--- components/features/Comment/Comment.tsx
+++ components/features/Comment/Comment.tsx
@@ -1,14 +1,24 @@
 'use client';
-import { useCommentSelector } from '@/hooks';
+import { FaTrash } from 'react-icons/fa6';
+
+import { Button, Confirm } from '@/components';
+import { useCommentSelector, useDisclosure, useMember } from '@/hooks';
+import { useCommentDelete } from '@/request-hooks';
 import { formatDate } from '@/utils';
 
 
 const Comment = () => {
+    const { trigger: deleteComment, isMutating: isDeleting } = useCommentDelete();
+    const { isOpen, close, open } = useDisclosure();
+    const { isAdmin } = useMember();
+    const { member } = useMember();
+
     const commentText = useCommentSelector(comment => comment.text);
     const commentCreatedAt= useCommentSelector(comment => comment.createdAt);
+    const commentAuthorId= useCommentSelector(comment => comment.author?.id);
     const commentFirstName= useCommentSelector(comment => comment.author?.firstName);
     const commentLastName= useCommentSelector(comment => comment.author?.lastName);
-
+    
     return (
         <div className='divide-x-2 mb-2'>
             <div className='text-sm italic'>
@@ -23,8 +33,31 @@ const Comment = () => {
                     )
                     : null}
             </div>
-            <p className='p-1'>{commentText}</p>
-            <div className=" m-0" />
+            <div className='flex justify-between'>
+                <p className='p-1'>{commentText}</p>
+                {(isAdmin || member.userId === commentAuthorId)
+                    ? (
+                        <>
+                            <Confirm
+                                isLoading={isDeleting}
+                                isOpen={isOpen}
+                                text="Are you sure you want to delete this comment?"
+                                onClose={close}
+                                onConfirm={() => deleteComment()}
+                            />
+                            <Button
+                                aria-label='delete comment'
+                                color='error' 
+                                isLoading={isDeleting} 
+                                size='xs' 
+                                onClick={open}
+                            >
+                                <FaTrash />
+                            </Button>
+                        </>
+                    )
+                    : null}
+            </div>
         </div>
     );
 };
