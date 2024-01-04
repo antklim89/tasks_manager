34ef27e16cf86b943564106ece0ce01320f8aa02
Anton <antklim89@mail.ru>
2024-01-04 20:19:20 +0300

    chore: remove resize event

--- components/features/Project/Project.tsx
+++ components/features/Project/Project.tsx
@@ -1,37 +1,17 @@
 'use client';
-import { useEffect, useState } from 'react';
-
 import { Column, TaskDndContext } from '@/components';
 import { ColumnContext } from '@/hooks';
 import { useColumnsFetch } from '@/request-hooks';
-import { cn } from '@/utils';
 
 import ProjectCreateColumn from './ProjectCreateColumn';
 
 
 const Project = () => {
     const { data: columns = [] } = useColumnsFetch();
-    const [ref, setRef] = useState<HTMLDivElement|null>(null);
-    const [isWidthOverflow, setIsWidthOverflow] = useState(false);
-    const [isHeightOverflow, setIsHeightOverflow] = useState(false);
-
-    useEffect(() => {
-        if (!ref) return () => null;
-        const listener = () => {
-            setIsWidthOverflow(ref.scrollWidth > document.body.clientWidth);
-            setIsHeightOverflow(ref.scrollHeight > document.body.clientHeight);
-        };
-        listener();
-        window.addEventListener('resize', listener);
-        return () => window.removeEventListener('resize', listener);
-    }, [ref]);
 
     return (
         <TaskDndContext>
-            <div
-                className={cn('flex flex-grow items-start gap-2', { 'overflow-x-scroll': isWidthOverflow, 'overflow-y-scroll': isHeightOverflow })} 
-                ref={setRef}
-            >
+            <div className="flex flex-grow items-start gap-2 overflow-scroll">
                 {columns.map((column) => (
                     <ColumnContext.Provider key={column.id} value={column}>
                         <Column />
